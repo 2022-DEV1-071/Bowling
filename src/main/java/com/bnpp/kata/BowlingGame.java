7dd3099d266f77976df9c1e26c34ca4a7a87c50c
2022-DEV1-071 <jhanu.special19@gmail.com>
2022-11-06 22:23:05 +0530

    Added no spare and no strike case

---
+++ src/main/java/com/bnpp/kata/BowlingGame.java
@@ -0,0 +1,12 @@
+package com.bnpp.kata;
+
+import static java.util.Arrays.stream;
+
+class BowlingGame {
+
+	private static final String noScore = "";
+
+	int calculateScore(String input) {
+		return stream(input.split(noScore)).mapToInt(Integer::parseInt).sum();
+	}
+}
---
+++ src/test/java/com/bnpp/kata/BowlingGameTest.java
@@ -0,0 +1,19 @@
+package com.bnpp.kata;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class BowlingGameTest {
+
+	@Test
+	public void noSpareAndNoStrike() throws Exception {
+		BowlingGame bowlingGame = new BowlingGame();
+
+		int score = bowlingGame.calculateScore("12345123451234512345");
+
+		assertThat(score, is(0));
+	}
+
+}
